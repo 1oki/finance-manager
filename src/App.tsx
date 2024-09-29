c284c7d9c9e58d6d49c6e01d2b5c73201edbdcad
Sergey Filippov <933117@gmail.com>
2024-09-29 16:52:00 +0700

    layout styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,65 @@ import AddTransactionForm from "./components/AddTransactionForm";
 import TransactionList from "./components/TransactionList";
 import Charts from "./components/Charts";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Personal Finance Manager</h1>
-      <AddTransactionForm />
-      <TransactionList />
-      <Charts />
-    </div>
-  );
+import { Flex, Layout } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  height: 90,
+  paddingInline: 48,
+  lineHeight: '64px',
+  backgroundColor: '#4096ff',
+  // padding: "10px",
 };
 
+const contentStyle: React.CSSProperties = {
+  paddingLeft: 20,
+  paddingRight: 20,
+  textAlign: 'left',
+  minHeight: 120,
+  lineHeight: '60px',
+  color: '#fff',
+  backgroundColor: '#0958d9',
+  fontSize: 20,
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: 'center',
+  lineHeight: '120px',
+  color: '#fff',
+  backgroundColor: '#1677ff',
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  color: '#fff',
+  backgroundColor: '#4096ff',
+};
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: 'hidden',
+  width: '100%',
+  maxWidth: '100%',
+};
+
+const App: React.FC = () => (
+  <div>
+    <Layout style={layoutStyle}>
+      <Header style={headerStyle}>
+        <h1>Personal Finance Manager</h1>       
+      </Header>
+      <Content style={contentStyle}>
+        <AddTransactionForm />
+        <TransactionList />
+        {/* <Charts /> */}
+      </Content>
+      <Footer style={footerStyle}>Footer</Footer>
+    </Layout>
+  </div>
+);
+
 export default App;
+
--- src/components/AddTransactionForm.tsx
+++ src/components/AddTransactionForm.tsx
@@ -3,6 +3,10 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useDispatch } from "react-redux";
 import { addTransaction } from "../redux/slices/transactionsSlice";
+import { Flex, Radio, Button } from "antd";
+
+// import { Button, Flex, Segmented } from 'antd';
+import type { FlexProps, SegmentedProps } from 'antd';
 
 interface Transaction {
     id: string;
@@ -14,15 +18,29 @@ interface Transaction {
     date: string;
   }
 
+const boxStyle: React.CSSProperties = {
+  width: '100%',
+  height: 120,
+  borderRadius: 6,
+  border: '1px solid #40a9ff',
+};
+const fieldStyle: React.CSSProperties = {
+  width: '20%',
+  height: 30,
+  borderRadius: 6,
+  border: '1px solid #40a9ff',
+  // border: '1px solid red', borderRadius: '4px'
+};
+
 
 const AddTransactionForm = () => {
   const dispatch = useDispatch();
 
   const validationSchema = Yup.object({
-    amount: Yup.number().positive("Amount must be positive").required("Required"),
-    category: Yup.string().required("Required"),
-    type: Yup.string().oneOf(["income", "expense"]).required("Required"),
-    date: Yup.date().required("Required"),
+    amount: Yup.number().positive("Amount must be positive").required(" Required"),
+    category: Yup.string().required(" Required"),
+    type: Yup.string().oneOf(["income", "expense"]).required(" Required"),
+    date: Yup.date().required(" Required"),
   });
 
   return (
@@ -39,29 +57,38 @@ const AddTransactionForm = () => {
     >
       {() => (
         <Form>
-          <div>
-            <label htmlFor="type">Transaction Type</label>
-            <Field as="select" name="type">
-              <option value="income">Income</option>
-              <option value="expense">Expense</option>
-            </Field>
-          </div>
-          <div>
-            <label htmlFor="amount">Amount</label>
-            <Field name="amount" type="number" />
-            <ErrorMessage name="amount" />
-          </div>
-          <div>
-            <label htmlFor="category">Category</label>
-            <Field name="category" type="text" />
-            <ErrorMessage name="category" />
-          </div>
-          <div>
-            <label htmlFor="date">Date</label>
-            <Field name="date" type="date" />
-            <ErrorMessage name="date" />
-          </div>
-          <button type="submit">Add Transaction</button>
+          {/* <Flex style={boxStyle} justify="space-around" align="flex-start" vertical> */}
+            <div>
+              <label htmlFor="type">Transaction Type </label>
+              {/* <Field as="select" name="type" >
+                <option value="">Income</option>
+                <option value="expense">Expense</option>
+              </Field> */}
+              <Radio.Group block defaultValue="income" optionType="button" buttonStyle="solid" >
+                <Radio value="income">Income</Radio>
+                <Radio value="expense">Expense</Radio>
+              </Radio.Group>
+            </div>
+            <div>
+              <label htmlFor="amount">Amount </label>
+              <Field name="amount" type="number" />
+              <ErrorMessage name="amount"/>
+            </div>
+            <div>
+              <label htmlFor="category">Category </label>
+              <Field name="category" type="text" style={fieldStyle}/>
+              <ErrorMessage name="category" />
+            </div>
+            <div>
+              <label htmlFor="date">Date </label>
+              <Field name="date" type="date" style={fieldStyle}/>
+              <ErrorMessage name="date" />
+            </div>
+            <div>
+              <Button type="primary">Add Transaction</Button>
+            </div>
+            
+          {/* </Flex> */}
         </Form>
       )}
     </Formik>
